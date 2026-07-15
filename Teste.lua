1a4c4adbb69feadbeef681986672e647954efa67
Gabysbbs <gabrieldeoliveirasilva067@gmail.com>
2026-07-15 02:36:26 -0300

    Update Teste.lua

--- Teste.lua
+++ Teste.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Gabso7dabe/Pvp/refs/heads/main/SISTEMAKEYY.lua?token=GHSAT0AAAAAAECUUD2SOD4YOT7PJKZIDPE22SXDAXQ"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Gabso7dabe/Pvp/main/SISTEMAKEYY.lua"))()
